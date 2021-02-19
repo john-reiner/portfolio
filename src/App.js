c58c3159e1ded5c23b76e46594e0103f5b9b6076
john-reiner <johnreiner1990@gmail.com>
2021-02-19 09:01:09 -0500

    added a fetchUser function and set state to a user object.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import Landing from './components/Landing.js'
 
 function App() {
+
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const fetchUser = () => {
+    fetch('http://localhost:3001/users/1')
+    .then(response => response.json())
+    .then(user => setUser(user))
+  }
+  console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Landing name={user.first_name + " " + user.last_name} />
     </div>
   );
 }
